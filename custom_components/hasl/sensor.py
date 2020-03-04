39d87abfbf1d1b3923d764cdb851b8db0bfb469d
Dimman Ramone <dimitris@weirdtales.se>
2020-03-04 12:52:29 +0100

    Bugfix in if statement

--- custom_components/hasl/sensor.py
+++ custom_components/hasl/sensor.py
@@ -736,7 +736,7 @@ class SLDeparturesSensor(Entity):
                     icon = iconswitcher.get(traffictype, 'mdi:train-car')
                     if int(self._direction) == 0 or int(direction) \
                             == int(self._direction):
-                        if len(self._lines) == 0 or linenumber \
+                        if self._lines == [] or linenumber \
                                 in self._lines:
                             diff = self.parseDepartureTime(displaytime)
                             if diff < self._timewindow:

8c4b131fd7d8da9f86ff04464476317c9d3a662a
Paul Donald <itsascambutmailmeanyway@gmail.com>
2024-03-06 22:43:50 +0100

    3.1.3: bump the old APIs so they function until at least 2024-03-31

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [3.1.3] (2024-03-06)
+
+### Fixes
+Buying time: use the newer API URL until its shutdown at end of 2024-03 @systemcrash
+
 ## [3.1.2] (2024-03-06)
 
 ### Fixes
--- custom_components/hasl3/slapi/const.py
+++ custom_components/hasl3/slapi/const.py
@@ -1,11 +1,15 @@
-__version__ = '3.1.2'
+__version__ = '3.1.3'
 
 FORDONSPOSITION_URL = 'https://api.sl.se/fordonspositioner/GetData?' \
                       'type={}&pp=false&cacheControl={}'
 
-TRAFIKLAB_URL = 'https://api.sl.se/api2/'
+# old https://api.sl.se/api2 ceases to function on 2024-03-15
+TRAFIKLAB_URL = 'https://journeyplanner.integration.sl.se/v1/'
+# Due to technical reasons, this API is being replaced by SLs Deviations API and will completely stop working on 2024-03-31
 SI2_URL = TRAFIKLAB_URL + 'deviations.json?key={}&siteid={}&lineNumber={}'
+# Due to technical reasons, this API is being replaced by SLs Deviations API and GTFS Service alerts. It will stop working on 2024-03-31
 TL2_URL = TRAFIKLAB_URL + 'trafficsituation.json?key={}'
+# This API will be shut down at the end of March 2024. It is replaced by SL’s new transport API.
 RI4_URL = TRAFIKLAB_URL + 'realtimedeparturesV4.json?key={}&siteid={}' \
                           '&timeWindow={}'
 PU1_URL = TRAFIKLAB_URL + 'typeahead.json?key={}&searchstring={}' \

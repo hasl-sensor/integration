353e65dbd309c998050f99139dff2fcd7e0fa2c1
Mikalai Kukhta <mikalai.kukhta@gmail.com>
2024-03-16 23:58:43 +0100

    update legacy slapi endpoint base

--- custom_components/hasl3/slapi/const.py
+++ custom_components/hasl3/slapi/const.py
@@ -3,7 +3,7 @@ __version__ = '3.1.1'
 FORDONSPOSITION_URL = 'https://api.sl.se/fordonspositioner/GetData?' \
                       'type={}&pp=false&cacheControl={}'
 
-TRAFIKLAB_URL = 'https://api.sl.se/api2/'
+TRAFIKLAB_URL = 'https://journeyplanner.integration.sl.se/v1/'
 PU1_URL = TRAFIKLAB_URL + 'typeahead.json?key={}&searchstring={}' \
                           '&stationsonly=False&maxresults=25'
 RP3_URL = TRAFIKLAB_URL + 'TravelplannerV3_1/trip.json?key={}&originExtId={}' \
